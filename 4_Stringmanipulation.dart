d7fc3c5fd422b0f3790ba44e6c437654b416625d
Tuhin Subhra Samanta <tuhinssam@gmail.com>
2021-07-13 23:40:03 +0530

    Create 4_Stringmanipulation.dart
    String Manipulation

---
+++ 4_Stringmanipulation.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  String str = "\tMy Name is\n Tuhin\n";
+  
+  //length
+  print(str.length);
+  
+  // converts to uppercase
+  print(str.toUpperCase());
+  
+  // converts to lowercase
+  print(str.toLowerCase());
+  
+  //get the first occurence of patterns
+  str.indexOf('me');
+  
+  //removed trailing whitespaces
+  print(str.trim());
+  
+  
+}
