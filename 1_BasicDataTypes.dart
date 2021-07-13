9ed3d921e954b0f7c961092807cd24efa1ec3e0f
Tuhin Subhra Samanta <tuhinssam@gmail.com>
2021-07-13 22:50:18 +0530

    Create 1_BasicDataTypes.dart
    basic data types

---
+++ 1_BasicDataTypes.dart
@@ -0,0 +1,14 @@
+void main() {
+  //string type
+  String name = 'Tuhin';
+  //int type
+  int age = 33;
+  //boolean type
+  bool isStudent = true;
+  //double
+  double height = 5.6;
+  //dynamic type
+  dynamic data = null;
+  data = 'some additional info about me';
+  print('Name: '+name + ' Age: ' + age.toString() + ' Height: '+ height.toString() + ' IsStudent: '+isStudent.toString());
+}
