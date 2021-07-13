2740c87863e6d5559197e8a9196a99535e7a11d9
Tuhin Subhra Samanta <tuhinssam@gmail.com>
2021-07-13 23:14:50 +0530

    Create 3_ArrowFunction.dart
    arrow function

---
+++ 3_ArrowFunction.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  print(add(5,6));
+  print(addArrow(10,11));
+}
+
+int add (int n1, int n2){
+  return n1 + n2;
+}
+
+//this is same as above add function
+int addArrow(int n1, int n2) => n1 + n2;
